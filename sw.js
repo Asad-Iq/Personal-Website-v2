64eb63f48d805bb6036bcb726bbaca39f3fed441
Asad Iqbal <77069629+Asad-Iq@users.noreply.github.com>
2026-06-12 20:55:15 +0100

    caching update

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Bump CACHE_VERSION whenever you change precached files (CSS/HTML/icons)
  * so returning visitors get the update.
  */
-const CACHE_VERSION = "v2";
+const CACHE_VERSION = "v3";
 const PRECACHE = `precache-${CACHE_VERSION}`;
 const RUNTIME = `runtime-${CACHE_VERSION}`;
 
