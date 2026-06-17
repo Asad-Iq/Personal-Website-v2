2cdcc0e56fcc4780269cc0ce70ef8053dfd13e58
Asad Iqbal <77069629+Asad-Iq@users.noreply.github.com>
2026-06-17 14:24:57 +0100

    download link update

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Bump CACHE_VERSION whenever you change precached files (CSS/HTML/icons)
  * so returning visitors get the update.
  */
-const CACHE_VERSION = "v15.3";
+const CACHE_VERSION = "v15.4.0";
 const PRECACHE = `precache-${CACHE_VERSION}`;
 const RUNTIME = `runtime-${CACHE_VERSION}`;
 
