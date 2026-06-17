f7e61fa251d07a7555e415dad5f152841b1e6c6d
Asad Iqbal <77069629+Asad-Iq@users.noreply.github.com>
2026-06-17 15:43:09 +0100

    Arcade apk upload to site

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Bump CACHE_VERSION whenever you change precached files (CSS/HTML/icons)
  * so returning visitors get the update.
  */
-const CACHE_VERSION = "v15.5.1";
+const CACHE_VERSION = "v16";
 const PRECACHE = `precache-${CACHE_VERSION}`;
 const RUNTIME = `runtime-${CACHE_VERSION}`;
 
